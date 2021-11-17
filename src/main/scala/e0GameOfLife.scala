1ba968b760a295e41f9753ee806f4c62c4428a01
Christian D'Errico <christian.derrico@studio.unibo.it>
2021-11-17 11:29:20 +0100

    first example code improved

--- src/main/scala/e0GameOfLife.scala
+++ src/main/scala/e0GameOfLife.scala
@@ -7,6 +7,7 @@ import scala.concurrent.duration.{FiniteDuration, SECONDS}
 object e0GameOfLife extends App {
 
   case class Iteration(generation: Int, cells: Seq[Int])
+  case class CellCoordinates(row: Int, column: Int)
 
   val width: Int = 10
   val height: Int = 10
@@ -32,15 +33,24 @@ object e0GameOfLife extends App {
 
   val loop = Source.repeat(Iteration(0, List.empty)).throttle(1, FiniteDuration(1, SECONDS))
 
-  val NEIGHBOURS = 8
-  val NEIGHBOURS_SQUARE_GRID_SIDE = 3
+  val NUMBER_OF_NEIGHBOURS = 8
+  val NEIGHBOURS_SQUARE_GRID_SIDE_LENGHT = 3
   val MINIMUM_OF_ALIVE_NEIGHBOURS = 2
   val MAXIMUM_OF_ALIVE_NEIGHBOURS = 3
   val ALIVE = 1
   val DEAD = 0
 
-  def excludeNotAuthorizedNeighbour(cell: (Int, Int)): Boolean =
-    (cell._1 >= 0 && cell._1 < width) && (cell._2 >= 0 && cell._2 < height)
+  def excludeNeighboursOutOfTheGrid(cell: CellCoordinates): Boolean =
+    (cell.row >= 0 && cell.row < width) && (cell.column >= 0 && cell.column < height)
+
+  def getCellIndex(cell: CellCoordinates): Int =
+    cell.row + cell.column * width
+
+  def computeNeighbourCoordinates(cellIndex: Int, neighbourId: Int): CellCoordinates =
+    CellCoordinates(
+      (cellIndex % width) + (neighbourId % NEIGHBOURS_SQUARE_GRID_SIDE_LENGHT) - 1,
+      (cellIndex / width) + (neighbourId / NEIGHBOURS_SQUARE_GRID_SIDE_LENGHT) - 1
+    )
 
   val gameLoop = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
     import GraphDSL.Implicits._
@@ -51,28 +61,27 @@ object e0GameOfLife extends App {
     val firstGenInjector = Source.single(Iteration(0, initialState))
 
     val doGeneration = Flow[Iteration]
-      .flatMapConcat(i =>
-        Source(i.cells.indices).flatMapConcat(cellIndex =>
-          Source(0 to NEIGHBOURS)
-            .map(neighbourCellIndex => (
-              (cellIndex % width) + (neighbourCellIndex % NEIGHBOURS_SQUARE_GRID_SIDE) - 1,
-              (cellIndex / width) + (neighbourCellIndex / NEIGHBOURS_SQUARE_GRID_SIDE) - 1)
-            ).filter(excludeNotAuthorizedNeighbour)
-            .filter(cell => cell._1 + cell._2 * width != cellIndex)
-            .fold(0)((acc, point) => acc + i.cells(point._1 + point._2 * width))
+      .flatMapConcat(iteration =>
+        Source(iteration.cells.indices).flatMapConcat(cellIndex => //for every cell
+          Source(0 to NUMBER_OF_NEIGHBOURS)
+            .map(computeNeighbourCoordinates(cellIndex, _)) //get neighbours
+            .filter(excludeNeighboursOutOfTheGrid) //exclude neighbours out of the grid
+            .filter(neighbour => getCellIndex(neighbour) != cellIndex) //exclude cell itself
+            .fold(0)((numberOfAliveNeighbours, cell) =>
+              numberOfAliveNeighbours + iteration.cells(getCellIndex(cell))) //compute number of alive neighbours
             .map {
               case n if n < MINIMUM_OF_ALIVE_NEIGHBOURS || n > MAXIMUM_OF_ALIVE_NEIGHBOURS => DEAD
-              case n if n == MAXIMUM_OF_ALIVE_NEIGHBOURS || i.cells(cellIndex) == ALIVE => ALIVE
+              case n if n == MAXIMUM_OF_ALIVE_NEIGHBOURS || iteration.cells(cellIndex) == ALIVE => ALIVE
               case _ => DEAD
-            }
+            } //apply Game Of Life's Rules
         ).grouped(width * height)
-          .map(cells => Iteration(i.generation + 1, cells))
+          .map(cells => Iteration(iteration.generation + 1, cells))
       )
 
     loop ~> gateRight.in0
     gateRight.out ~> broadcast ~> printer
     gateRight.in1 <~ concat <~ firstGenInjector
-    concat <~ doGeneration <~ broadcast
+                     concat <~ doGeneration <~ broadcast
 
     ClosedShape
   })
