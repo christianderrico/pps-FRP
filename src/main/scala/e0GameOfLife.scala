5c826f404d19ad07c8ae92e522dac098256597b0
Christian D'Errico <christian.derrico@studio.unibo.it>
2021-11-26 16:18:09 +0100

    improve code for example

--- src/main/scala/e0GameOfLife.scala
+++ src/main/scala/e0GameOfLife.scala
@@ -1,17 +1,16 @@
-import akka.NotUsed
+import ReactiveGameOfLife.GameOfLife
+import ReactiveGameOfLife.GameOfLife.{Cells, Iteration, Position, defaultHeight}
 import akka.actor.ActorSystem
 import akka.stream.ClosedShape
 import akka.stream.scaladsl.{Broadcast, Concat, Flow, GraphDSL, RunnableGraph, Sink, Source, ZipWith}
+import akka.util.Index
 
-import scala.concurrent.duration.{FiniteDuration, SECONDS}
+import scala.concurrent.duration.{DurationInt, FiniteDuration, SECONDS}
 
 object e0GameOfLife extends App {
 
-  case class Iteration(generation: Int, cells: Seq[Int])
-  case class CellCoordinates(row: Int, column: Int)
-
-  val width: Int = 10
-  val height: Int = 10
+  val width = GameOfLife.defaultWidth
+  val height = GameOfLife.defaultHeight
 
   val initial = ".........." +
                 ".........." +
@@ -25,7 +24,7 @@ object e0GameOfLife extends App {
                 "..........";
 
   val isAlive: Char => Int = c => if (c == '#') 1 else 0
-  val initialState: Seq[Int] = initial.flatMap(v => Seq(v)).map(isAlive)
+  val initialState: Cells = initial.flatMap(v => Seq(v)).map(isAlive)
 
   val NUMBER_OF_NEIGHBOURS = 9
   val NEIGHBOURS_SQUARE_GRID_SIDE_LENGTH = 3
@@ -34,26 +33,17 @@ object e0GameOfLife extends App {
   val ALIVE = 1
   val DEAD = 0
 
-  def excludeNeighboursOutOfTheGrid(cell: CellCoordinates): Boolean =
-    (cell.row >= 0 && cell.row < width) && (cell.column >= 0 && cell.column < height)
-
-  def getCellIndex(cell: CellCoordinates): Int =
-    cell.row + cell.column * width
-
-  def computeNeighbourCoordinates(cellIndex: Int, neighbourId: Int): CellCoordinates =
-    CellCoordinates(
-      (cellIndex % width) + (neighbourId % NEIGHBOURS_SQUARE_GRID_SIDE_LENGTH) - 1,
-      (cellIndex / width) + (neighbourId / NEIGHBOURS_SQUARE_GRID_SIDE_LENGTH) - 1
-    )
-
   val gameLoop = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
     import GraphDSL.Implicits._
 
-    val loop = Source.repeat().throttle(1, FiniteDuration(1, SECONDS))
+    val loop = Source.repeat().throttle(1, 1.seconds)
 
     val printer = Sink.foreach[Iteration](i => {
       println("GENERATION: " + i.generation)
-      Source(i.cells).map(c => if (c > 0) "#" else ".").grouped(width).map(s => "".concat(s)).runForeach(println(_))
+      Source(i.cells).map(c => if (c > 0) "#" else ".")
+                     .grouped(width)
+                     .map(s => "".concat(s))
+                     .runForeach(println(_))
     })
 
     val gateRight = b.add(ZipWith((_:Unit, right: Iteration) => right))
@@ -61,18 +51,20 @@ object e0GameOfLife extends App {
     val concat = b.add(Concat[Iteration]())
     val firstGenInjector = Source.single(Iteration(0, initialState))
 
+    import Utilities._
+
     val doGeneration = Flow[Iteration]
       .flatMapConcat(iteration =>
-        Source(iteration.cells.indices).flatMapConcat(cellIndex => //for every cell
+        Source(iteration.cells.indices).flatMapConcat(referenceCellId => //for every cell
           Source(0 until NUMBER_OF_NEIGHBOURS)
-            .map(computeNeighbourCoordinates(cellIndex, _)) //get neighbours
+            .map(computeNeighboursPositionsOf(referenceCellId)) //get neighbours
             .filter(excludeNeighboursOutOfTheGrid) //exclude neighbours out of the grid
-            .filter(neighbour => getCellIndex(neighbour) != cellIndex) //exclude cell itself
-            .fold(0)((numberOfAliveNeighbours, cell) =>
-              numberOfAliveNeighbours + iteration.cells(getCellIndex(cell))) //compute number of alive neighbours
+            .filter(exclude(referenceCellId)) //exclude cell itself
+            .fold(0)((numberOfAliveNeighbours, neighbour) =>
+              numberOfAliveNeighbours + iteration.cells(neighbour.toCellsIndex))//compute number of alive neighbours
             .map {
               case n if n < MINIMUM_OF_ALIVE_NEIGHBOURS || n > MAXIMUM_OF_ALIVE_NEIGHBOURS => DEAD
-              case n if n == MAXIMUM_OF_ALIVE_NEIGHBOURS || iteration.cells(cellIndex) == ALIVE => ALIVE
+              case n if n == MAXIMUM_OF_ALIVE_NEIGHBOURS || iteration.cells(referenceCellId) == ALIVE => ALIVE
               case _ => DEAD
             } //apply Game Of Life's Rules
         ).grouped(width * height)
@@ -89,5 +81,21 @@ object e0GameOfLife extends App {
 
   implicit val system: ActorSystem = ActorSystem("QuickStart")
   gameLoop.run()
+
+}
+
+object Utilities {
+  import e0GameOfLife._
+
+  def excludeNeighboursOutOfTheGrid(cell: Position): Boolean =
+    (cell.row >= 0 && cell.row < width) && (cell.column >= 0 && cell.column < height)
+
+  def exclude(referenceCellId: Int)(neighbour: Position): Boolean = neighbour.toCellsIndex != referenceCellId
+
+  def computeNeighboursPositionsOf(cellIndex: Int)(neighbourId: Int): Position =
+    Position(
+      (cellIndex % width) + (neighbourId % NEIGHBOURS_SQUARE_GRID_SIDE_LENGTH) - 1,
+      (cellIndex / width) + (neighbourId / NEIGHBOURS_SQUARE_GRID_SIDE_LENGTH) - 1
+    )
 
 }
