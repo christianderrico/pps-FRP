9d9f5d9115eddda6a80a52e6a1bba10ab124bf2e
Christian D'Errico <christian.derrico@studio.unibo.it>
2021-11-28 08:32:08 +0100

    GameOfLife adt almost defined

--- src/main/scala/ReactiveGameOfLife/GameOfLife.scala
+++ src/main/scala/ReactiveGameOfLife/GameOfLife.scala
@@ -1,11 +1,17 @@
 package ReactiveGameOfLife
 
-object Model {
+object GameOfLife {
 
   type Cells = Seq[Int]
 
+  val defaultWidth: Int = 10
+  val defaultHeight: Int = 10
+
   case class Iteration(generation: Int, cells: Cells)
-  case class CellCoordinates(row: Int, column: Int)
+  case class Position(row: Int, column: Int) {
+    private def convertToIndex(width: Int = defaultWidth): Int = row + column * width
+    val toCellsIndex = convertToIndex()
+  }
 
 }
 
