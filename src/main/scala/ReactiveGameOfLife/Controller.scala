a299474adf6a48a95e022c998214e4657e83a6e9
Christian D'Errico <christian.derrico@studio.unibo.it>
2021-11-29 04:54:02 +0100

    controller updated to get view cells input

--- src/main/scala/ReactiveGameOfLife/Controller.scala
+++ src/main/scala/ReactiveGameOfLife/Controller.scala
@@ -1,44 +1,30 @@
 package ReactiveGameOfLife
 
-import akka.stream.{ClosedShape, Graph}
-import akka.stream.scaladsl.{GraphDSL, RunnableGraph, Source, ZipWith}
+import ReactiveGameOfLife.View.Start
+import monix.eval.Task
+import monix.reactive.Observable
 
-import scala.concurrent.duration.{DurationInt, FiniteDuration, MILLISECONDS, TimeUnit}
+import scala.concurrent.duration.DurationInt
+
+case class Controller(view: View){
+
+  val loop = Observable.interval(33.millis)
+
+  def getNext(accumulator: Int, next: Int): Int = accumulator match {
+    case 0 => next + 1
+    case _ => accumulator + 1
+  }
+
+  def gameLoop: Task[Unit] = Observable.combineLatest2(view.startAndStopInput, loop/*, view.cellsInput*/)
+                                       .collect {
+                                         case (Start, _) => 0
+                                       }
+                                       .scan(0)(getNext)
+                                       .foreachL(println)
+}
 
 object Controller {
 
-  case class Config(sleepTime: FiniteDuration = 16.millis)
-
-  def apply(config: Config): Unit = {
-    RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
-      import GraphDSL.Implicits._
-      import Utilities.Implicits._
-
-      /*val timeSleepCalculator = b.add(ZipWith(
-        (t1: FiniteDuration, t2: FiniteDuration) => FiniteDuration(t1.time - t2.time, MILLISECONDS))
-      )*/
-
-      def waitForNextElement(standardTime: FiniteDuration, deltaTime: FiniteDuration) =
-        if(deltaTime.time > standardTime.time)
-          standardTime
-        else
-          standardTime - deltaTime
-
-
-      /*val engine = (Source.repeat()
-                           >>= (_ => println("Process Input"))
-                           >>= (_ => getClock)
-                           >>= (prevTime => { println("update model"); prevTime })
-                           >>= (prevTime => (prevTime, getClock))
-                           >>= (t => t._1 - t._2)
-        )*/
-
-      //engine ~> timeSleepCalculator
-
-      ClosedShape
-    })
-  }
-
-  private val getClock = FiniteDuration(System.currentTimeMillis(), MILLISECONDS)
+  def apply(view: View): Controller = new Controller(view)
 
 }
