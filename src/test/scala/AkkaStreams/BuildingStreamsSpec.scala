eefca48dfd18278ac6bc318dd2519e52a1250190
Christian D'Errico <christian.derrico@studio.unibo.it>
2021-12-11 09:04:56 +0100

    Added new examples for graph DSL

--- src/test/scala/AkkaStreams/BuildingStreamsSpec.scala
+++ src/test/scala/AkkaStreams/BuildingStreamsSpec.scala
@@ -1,6 +1,8 @@
 package AkkaStreams
 
-import akka.stream.scaladsl.Source
+import akka.Done
+import akka.stream.{CompletionStrategy, KillSwitches, OverflowStrategy}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import org.reactivestreams.Subscriber
 
 import scala.concurrent.Await
@@ -10,26 +12,31 @@ class BuildingStreamsSpec extends BaseSpec {
 
   "An Akka Stream " should {
 
-    val canBeBuilt = afterWord("can be built")
+    val beComposed = "be composed "
+    val canBeBuiltAndMaterialized = afterWord("can be built and materialized")
 
-    "be composed by a Source " which canBeBuilt {
+    s"$beComposed by a couple Source-Sink " which canBeBuiltAndMaterialized {
 
       "from Iterable" in {
+        val rangeStart = 1
+        val rangeLimit = 10_000
+        val source = Source(rangeStart to rangeLimit)
 
-        val source = Source(1 to 10_000)
-
-        Await.result(getElementsFromSource(source), 1.seconds) shouldBe (1 to 10_000)
+        Await.result(source.runWith(Sink.seq), 1.seconds) shouldBe (rangeStart to rangeLimit)
       }
 
       "from a single value" in {
 
         val source = Source.single(0)
 
-        Await.result(getFirstElementFromSource(source), 1.seconds) shouldBe 0
+        Await.result(source.runWith(Sink.head), 1.seconds) shouldBe 0
       }
 
+      val rangeStart = 0
+      val rangeLimit = 10
+
       val sourceFromPublisher = Source.fromPublisher((s: Subscriber[_ >: Int]) => {
-        for (i <- 0 until 10) {
+        for (i <- rangeStart until rangeLimit) {
           s.onNext(i)
         }
         s.onComplete()
@@ -37,19 +44,54 @@ class BuildingStreamsSpec extends BaseSpec {
 
       "from a Publisher " in {
 
-        //source is not backpressuring upstream
+        //publisher doesn't backpressure source
         val result1 = getElementsFromSource(sourceFromPublisher)
         assertThrows[IllegalStateException](Await.result(result1, 1.seconds))
 
+        //timed-based flow control
         val result2 = getElementsFromSource(sourceFromPublisher.throttle(1, 100.millis))
         val seq = Await.result(result2, 1.seconds)
 
-        seq shouldBe (0 until 10)
+        seq shouldBe (rangeStart until rangeLimit)
       }
 
     }
 
+    val can = afterWord("can")
 
+    s"$beComposed by 3-element tuple of Source-Sink-Flow " which can {
+
+      val startRange = 1
+      val endRange = 200
+
+      val source = Source[Int](startRange to endRange)
+
+      "be plugged together to build a Graph Stream " in {
+
+        val squarePow = 2
+
+        val square = Flow[Int].map(v => Math.pow(v, squarePow).toInt)
+        val sink = Sink.last[Int]
+
+        val result = source.via(square).toMat(sink)(Keep.right).run()
+
+        val lastSquare = Await.result(result, 1.seconds)
+
+        lastSquare shouldBe Math.pow(endRange, squarePow)
+      }
+
+      "be manipulated controlling materialized values " in {
+
+        val timedSource = Source.tick(0.seconds, 0.1.seconds, empty)
+        val sum = Flow[Int].fold(0)((acc, value) => acc+value).zipWith(timedSource)(Keep.left)
+        val sink = Sink.head[Int]
+
+        val result = source.viaMat(sum)(Keep.right).toMat(sink)(Keep.right).run()
+
+        Await.result(result, 1.seconds) shouldBe (endRange * (endRange + 1) / 2)
+      }
+
+    }
 
   }
 
---
+++ src/test/scala/AkkaStreams/GraphDSLSpec.scala
@@ -0,0 +1,120 @@
+package AkkaStreams
+
+import akka.stream.{ClosedShape, FlowShape, Outlet, SinkShape, SourceShape}
+import akka.stream.scaladsl.{Balance, Broadcast, Concat, Flow, GraphDSL, Keep, Merge, RunnableGraph, Sink, Source, ZipWith}
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.util.Random
+
+class GraphDSLSpec extends BaseSpec {
+
+  val allow = afterWord("allow")
+
+  "Graph DSL" should allow {
+
+    "to build complex graph using Sources, Flows, Sinks and junctions" in {
+
+      val sink = Sink.head[Int]
+
+      val start = 100
+      val graph = RunnableGraph.fromGraph(GraphDSL.createGraph(sink){ implicit builder =>
+        sink =>
+
+        import GraphDSL.Implicits._
+
+        val sourceA = Source[Int](0 to 100)
+        val sourceB = Source[Int](start to 200)
+
+        val flowA = Flow[Int].map(_ * 2)
+        val followedflowB = Flow[Int].map(_ / 2)
+
+        val zip = builder.add(ZipWith[Int, Int, Int](Keep.right))
+        val outputPorts = 2
+        val broadcastA = builder.add(Broadcast[Int](outputPorts))
+
+        val flowC = Flow[Int].filter(_ % 2 == 0)
+
+        val broadcastB = builder.add(Broadcast[Int](outputPorts))
+
+        val followedFlowD = Flow[Int].map(_ * 5)
+        val flowE = Flow[Int].map(_.toString)
+
+        val zip2 = builder.add(ZipWith[Int, String, Int](Keep.left))
+        val zip3 = builder.add(ZipWith[Int, Int, Int](Keep.right))
+
+        sourceA ~>     flowA     ~> zip.in0
+        sourceB ~> followedflowB ~> zip.in1
+                                    zip.out ~> broadcastA.in
+                                               broadcastA.out(0) ~> broadcastB.in
+                                                                    broadcastB.out(0) ~> flowC ~>             zip3.in0
+                                                                    broadcastB.out(1) ~> flowE ~> zip2.in1
+                                               broadcastA.out(1) ~>   followedFlowD   ~>          zip2.in0
+                                                                                                  zip2.out ~> zip3.in1
+                                                                                                              zip3.out ~> sink
+
+
+        ClosedShape
+      })
+
+      val secondStep: Int => Int = _ / 2
+      val thirdStep: Int => Int = _ * 5
+
+      Await.result(graph.run(), 1.seconds) shouldBe thirdStep(secondStep(start))
+    }
+
+    "to create complex data pipelines using complex base block" in {
+      val complexSource = Source.fromGraph(GraphDSL.create(){ implicit builder =>
+       import GraphDSL.Implicits._
+
+       val upperBound = 100
+       val n = Random.nextInt(upperBound)
+
+       val sourceA = Source.single(2 * n)
+       val sourceB = Source.single(2 * n + 1)
+
+       val zip = builder.add(ZipWith[Int, Int, Double](_+_))
+       val flow = Flow[Double].map(_ * 2)
+       val merge = builder.add(Merge[Double](1))
+
+       sourceA ~> zip.in0
+       sourceB ~> zip.in1
+                  zip.out ~> flow ~> merge
+
+       SourceShape(merge.out)
+     })
+
+      val complexFlow = Flow.fromGraph(GraphDSL.create(){ implicit builder =>
+       import GraphDSL.Implicits._
+
+       val outPorts = 2
+       val broadcast = builder.add(Broadcast[Double](outPorts))
+
+       val flowA = Flow[Double].filter(_ % 2 == 0).map(_ * 0)
+       val flowB = Flow[Double].map(_ * 2)
+
+       val zip = builder.add(ZipWith[Double, Double, Double]((v1, v2) => Math.min(v1, v2)))
+
+       broadcast.out(0) ~> flowA ~> zip.in0
+       broadcast.out(1) ~> flowB ~> zip.in1
+
+       FlowShape(broadcast.in, zip.out)
+     })
+
+      //fluid DSL
+      val complexSink = {
+        val mult = Random.nextInt()
+        Flow[Double].map(_ * mult)
+          .toMat(Sink.head[Double])(Keep.right)
+      }
+
+      val res = complexSource
+                  .viaMat(complexFlow)(Keep.right)
+                  .toMat(complexSink)(Keep.right).run()
+
+      Await.result(res, 1.seconds) shouldBe 0
+
+    }
+  }
+
+}
